75216a65755e582088988f2ce7e2afe82eccee93
tituszobody <136613920+tituszobody@users.noreply.github.com>
2023-06-26 14:08:13 -0500

    Add print messages function

--- hello.py
+++ hello.py
@@ -1,2 +1,7 @@
 print("Hello World!")
 print("\t THis is my first Git repository on Github.")
+def print_message(name='Python'):
+    print(f"Welcome to the class from {name}!)
+
+name = "Julia"
+print_message(name)
